7ccc2e1be05f025cd71647cf08e02c4f58c57c22
IT20642914 <avishkachanaka@gmail.com>
2024-04-20 07:07:45 +0530

    Update Header component to include new route constants

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { logo } from "../../assets/images";
+import { APP_ROUTES } from '../../utilities/constants';
 
 const Header = () => {
   return (
@@ -10,13 +11,13 @@ const Header = () => {
         <img src={logo} alt="Logo" style={{ height: '80px' }} /> 
       <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
       </Typography>
-      <Button color="inherit" component={Link} to="/">
+      <Button color="inherit" component={Link} to={APP_ROUTES.USER_FEEDBACK_VIEW}>
         Home
       </Button>
-      <Button color="inherit" component={Link} to="/">
+      <Button color="inherit" component={Link} to={APP_ROUTES.USER_FEEDBACK_VIEW}>
         About
       </Button>
-      <Button color="inherit" component={Link} to="/">
+      <Button onClick={()=>{localStorage.clear()}}  color="inherit" component={Link} to={APP_ROUTES.ROOT}>
         Log Out
       </Button>
     </Toolbar>
